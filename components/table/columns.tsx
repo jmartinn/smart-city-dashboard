f442c36fa350984ea859d9cd2394c27dc25cbadb
Juan Pedro Martin <juamp_m@hotmail.com>
2024-12-02 13:27:49 +0100

    feat(table): add energy consumption column definitions
    - Create column definitions for id, date, consumption, production, and
      emissions
    - Implement DataTableRowActions component for row-level operations
    
    Note: Actions defined in the `DataTableRowActions` component are not
    functional yet, this is just a basic implementation.

---
+++ components/table/columns.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+
+import type { EnergyData } from '@/schemas/consumption';
+
+import { DataTableColumnHeader } from './data-table-column-header';
+import { DataTableRowActions } from './data-table-row-actions';
+
+export const columns: ColumnDef<EnergyData>[] = [
+  {
+    accessorKey: 'id',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Id" />
+    ),
+    cell: ({ row }) => <div>{row.getValue('id')}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'date',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Date" />
+    ),
+    enableSorting: true, // NOTE: This might be tricky to sort.
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'consumption',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Consumption" />
+    ),
+    enableSorting: true,
+    enableHiding: false,
+    cell: ({ row }) => <div>{row.getValue('consumption')} kWh</div>,
+  },
+  {
+    accessorKey: 'production',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Production" />
+    ),
+    enableSorting: true,
+    enableHiding: false,
+    cell: ({ row }) => <div>{row.getValue('production')} kWh</div>,
+  },
+  {
+    accessorKey: 'emissions',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Emissions" />
+    ),
+    enableSorting: true,
+    enableHiding: false,
+    cell: ({ row }) => (
+      <div>
+        {row.getValue('emissions')} kg CO<sub>2</sub>
+      </div>
+    ),
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => <DataTableRowActions row={row} />,
+  },
+];
---
+++ components/table/data-table-row-actions.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { Row } from '@tanstack/react-table';
+import { MoreHorizontal } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuLabel,
+} from '@/components/ui/dropdown-menu';
+import { energySchema } from '@/schemas/consumption';
+
+interface DataTableRowActionsProps<TData> {
+  row: Row<TData>;
+}
+
+export function DataTableRowActions<TData>({
+  row,
+}: DataTableRowActionsProps<TData>) {
+  const data = energySchema.parse(row.original);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex size-8 p-0 data-[state=open]:bg-muted"
+        >
+          <MoreHorizontal />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem
+          onClick={() => navigator.clipboard.writeText(data.id.toString())}
+        >
+          Copy ID
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        {/* TODO: Handle 'View details' route / page*/}
+        <DropdownMenuItem>View details</DropdownMenuItem>
+        {/* NOTE: This can be deleted later as we don't plan to interact with the data*/}
+        <DropdownMenuItem>Edit data</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
