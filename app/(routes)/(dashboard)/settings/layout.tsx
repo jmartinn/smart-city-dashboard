a51dade93d90042d1954a3c7e025ed01450bd287
Juan Pedro Martin <juamp_m@hotmail.com>
2024-12-09 01:41:22 +0100

    refactor(settings): refine settings layout and metadata
    - Update metadata title to 'Settings'.
    - Adjust layout for improved spacing and responsiveness.
    - Increase child content max width to `lg:max-w-3xl`.

--- app/(routes)/(dashboard)/settings/layout.tsx
+++ app/(routes)/(dashboard)/settings/layout.tsx
@@ -1,13 +1,11 @@
 import { Metadata } from 'next';
-import Image from 'next/image';
 
 import { Separator } from '@/components/ui/separator';
 
 import { SidebarNav } from './components/sidebar-nav';
 
 export const metadata: Metadata = {
-  title: 'Forms',
-  description: 'Advanced form example using react-hook-form and Zod.',
+  title: 'Settings',
 };
 
 const sidebarNavItems = [
@@ -31,8 +29,8 @@ interface SettingsLayoutProps {
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
-    <>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
+    <div className="container mx-auto px-4 py-6 sm:px-6 lg:px-8">
+      <div className="space-y-6">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
@@ -41,12 +39,12 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
+          <aside className="lg:w-1/4">
             <SidebarNav items={sidebarNavItems} />
           </aside>
-          <div className="flex-1 lg:max-w-2xl">{children}</div>
+          <div className="flex-1 lg:max-w-3xl">{children}</div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
