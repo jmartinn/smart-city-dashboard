98ed7e07f22580365eb7ce7400dce181492cd2ca
Juan Pedro Martin <juamp_m@hotmail.com>
2024-12-11 18:26:22 +0100

    chore: remove unused mock data function from utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,13 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function generateMockData(count: number) {
-  return Array.from({ length: count }, (_, i) => ({
-    id: i + 1,
-    date: new Date(2023, 0, 1 + i).toISOString().split('T')[0],
-    consumption: Math.floor(Math.random() * 500) + 1000,
-    production: Math.floor(Math.random() * 500) + 900,
-    emissions: Math.floor(Math.random() * 200) + 400,
-  }));
-}
